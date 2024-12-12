e770b543925c313eea078004f39da8fe9519f011
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-12-12 19:54:46 +0530

    Add the db url.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import { app } from "./app.js";
 import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
 dotenv.config(
     { path: "./.env" }
 )
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is running on port " + process.env.PORT);
-});
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.error("Error: ", error)
+        })
+
+        app.listen(process.env.PORT || 5000, () => {
+            console.log("Server is running on port " + process.env.PORT);
+        });
+    }).catch((error) => console.error("DB connection failed: ", error))
+
+
 
 
 app.get("/", (req, res) => {
