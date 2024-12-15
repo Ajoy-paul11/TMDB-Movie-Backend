0af2b5d363e4407075869142caaf8ac5d632261a
Ajoy-paul11 <ajoypaul8875@gmail.com>
2024-12-16 00:21:29 +0530

    add the interpolation to the route.

--- src/server.js
+++ src/server.js
@@ -21,9 +21,9 @@ connectDB()
 
 
 app.get("/", (req, res) => {
-    return res.send(<div>
+    res.send(`<div>
         <h1 style={{ textAlign: "center", marginBottom: "16px" }}>Hello from Movie Backend</h1>
         <a style={{ textAlign: "center" }} href="https://github.com/Ajoy-paul11/TMDB-Movie-Backend" target="_blank" >GitHub Repo</a>
     </div>
-    );
+    `);
 });
